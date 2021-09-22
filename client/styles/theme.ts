c98a9090c4a4cf2e611dc09c6249cb13618adc39
YoujinCha <s20141232s@gmail.com>
2021-09-22 17:15:37 +0900

    Setting global color, theme

---
+++ client/styles/theme.ts
@@ -0,0 +1,13 @@
+export default {
+  color: {
+    blue: '#0d46a0',
+    darkBlue: '#002071',
+    lightBlue: '#5471d2',
+    yellow: '#fdd835',
+    lightYellow: '#ffff6b',
+    gray: '#8f8f8f',
+    lightGray: '#dddddd',
+    black: '#212121',
+    white: '#ffffff',
+  },
+};
