844a2f1bb0e2854ad4db2d4fb3c33251f6789728
차유진 <s20141232s@gmail.com>
2021-10-10 15:21:59 +0900

    [CHORE] img 추가, Formik 수정

--- client/components/common/FormRadiobtn.tsx
+++ client/components/common/FormRadiobtn.tsx
@@ -3,27 +3,44 @@ import ReactDOM from 'react-dom';
 import { Formik, Field, Form, FormikHelpers } from 'formik';
 
 interface Values {
-  resumeForm: 'Eng' | 'Kor';
-  resumeTemplate: number;
+  resumeForm: 'Kor' | 'Eng';
 }
 
 // label value이랑 img src 배열로 주면
 // map 해서 라디오 그룹 만들어주는 컴포넌트
 
-const FormRadioBtn = () => {
+export const FormRadioBtn = () => {
   return (
     <>
-      <h1>FormRadio Test</h1>
-      {Children}
-      <Formik
-        initialValues={{
-          resumeForm: 'Kor',
-          resumeTemplate: 0,
-        }}
-        onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
-          console.log(values);
-        }}
-      ></Formik>
+      <div>
+        <Formik
+          initialValues={{
+            picked: 'Kor',
+          }}
+          onSubmit={async (values) => {
+            await new Promise((r) => setTimeout(r, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          {({ values }) => (
+            <Form>
+              <div id="my-radio-group">Picked</div>
+              <div role="group" aria-labelledby="my-radio-group">
+                <label>
+                  <Field type="radio" name="picked" value="Kor" />
+                  국문
+                </label>
+                <label>
+                  <Field type="radio" name="picked" value="Eng" />
+                  영문
+                </label>
+                <div>Picked: {values.picked}</div>
+              </div>
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </>
   );
 };
--- client/pages/Resume/index.tsx
+++ client/pages/Resume/index.tsx
@@ -1,12 +1,16 @@
 import { FunctionComponent } from 'react';
 
+// Todo : login 상태 받아오기
+// 로그인 되어 있다면 step1으로 routing
+// 로그인 되어있지 않다면 SignUp 페이지로 routing
+
 const WriteResume: FunctionComponent = () => {
   return <div>Write Resume page</div>;
 };
 
 // fetch하거나 caching 할 데이터가 없는데 이걸 굳이 써야하는지 의문 🤔
-export async function getStaticProps() {
+// export async function getStaticProps() {
 
-}
+// }
 
 export default WriteResume;
